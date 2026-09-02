5fdba293c362192a1652892e3bfa2b0dfec4b365
rddckdeo <rddckdeo@naver.com>
2026-09-02 16:27:46 +0900

    feat : project detauls - 1

--- src/sections/Projects/ProjectComponents.jsx
+++ src/sections/Projects/ProjectComponents.jsx
@@ -7,18 +7,18 @@ export function ProjectComponentsLeft({projectDetails}) {
             <div className='project-modal-face project-modal-back'>
                 <p className='text-heading' style={{ fontSize: '26px' }}>Description</p>
                 <div className='project-modal-left-content'>
-                    <small className='project-modal-label'>설명</small>
-                    <p className='project-modal-body'>본문</p>
+                    <small className='project-modal-label'>{projectDetails.subTitle}</small>
+                    <p className='project-modal-body'>{projectDetails.content}</p>
 
                     <hr className='project-modal-divider' />
 
                     <div className='project-modal-info-row'>
                         <p className='project-modal-info-title'>기간.인원</p>
-                        <small className='project-modal-info-value'>기간 및 인원</small>
+                        <small className='project-modal-info-value'>{projectDetails.periodAndTeam}</small>
                     </div>
                     <div className='project-modal-info-row'>
                         <p className='project-modal-info-title'>Git Hub Link</p>
-                        <small className='project-modal-info-value project-modal-link'>누르면 링크 이동</small>
+                        <small className='project-modal-info-value project-modal-link'>{projectDetails.github}</small>
                     </div>
 
                     <small className='project-modal-footnote'>프로젝트의 더 자세한 내용은 Git Hub Link를 눌러주세요!</small>
@@ -31,8 +31,8 @@ export function ProjectComponentsLeft({projectDetails}) {
 export function ProjectComponentsCenter({projectDetails}) {
     return (
         <div className='project-modal-center-content'>
-            <p className='project-modal-center-title'>Project Name</p>
-            <div className='project-modal-center-image'>사진</div>
+            <p className='project-modal-center-title'>{projectDetails.title}</p>
+            <div className='project-modal-center-image'><img src={projectDetails.picture[0]} width={250} alt=''/></div>
             <div className='project-modal-center-image'>사진</div>
         </div>
     )
@@ -48,23 +48,31 @@ export function ProjectComponentsRight({projectDetails}) {
                 <div className='project-modal-right-content'>
                     <small className='project-modal-label'>Skills</small>
                     <div className='project-modal-tag-group'>
-                        여기에 아이콘 들어갈 예정
-                        {/* <div className='project-modal-tag-group'>
-                            {skills.map((skill, index) => (
+                        <div className='project-modal-tag-group'>
+                            {projectDetails.skills.map((skill, index) => (
                                 <span key={index} className='project-modal-tag'>{skill}</span>
                             ))}
-                        </div> */}
+                        </div>
                     </div>
 
                     <small className='project-modal-label'>Tools</small>
                     <div className='project-modal-tag-group'>
-                        여기에 아이콘 들어갈 예정
+                        <div className='project-modal-tag-group'>
+                            {projectDetails.tools.map((tool, index) => (
+                                <span key={index} className='project-modal-tag'>{tool}</span>
+                            ))}
+                        </div>
                     </div>
 
                     <hr className='project-modal-divider' />
 
                     <div className='project-modal-role'>
                         <p className='text-heading' style={{ fontSize: '20px' }}>Role</p>
+                        <div className='project-modal-tag-group'>
+                            {projectDetails.roles.map((role, index) => (
+                                <span key={index} className='project-modal-tag'>{role}</span>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/sections/Projects/projectDetails.js
+++ src/sections/Projects/projectDetails.js
@@ -1,4 +1,18 @@
 // project 데이터 모음
+import haru1 from '../../assets/images/projects/haru/haru1.png'
+import haru2 from '../../assets/images/projects/haru/haru2.png'
+import haru3 from '../../assets/images/projects/haru/haru3.png'
+import haru4 from '../../assets/images/projects/haru/haru4.png'
+import haru5 from '../../assets/images/projects/haru/haru5.png'
+
+import CoCo1 from '../../assets/images/projects/CoCo/CoCo1.png'
+import CoCo2 from '../../assets/images/projects/CoCo/CoCo2.png'
+import CoCo3 from '../../assets/images/projects/CoCo/CoCo3.png'
+import CoCo4 from '../../assets/images/projects/CoCo/CoCo4.png'
+import CoCo5 from '../../assets/images/projects/CoCo/CoCo5.png'
+import CoCo6 from '../../assets/images/projects/CoCo/CoCo6.png'
+
+
 export const projectDetails = [
     { // Haru
         title : "Hotel Haru",
@@ -6,14 +20,12 @@ export const projectDetails = [
         content: "초특가 여기어때 프로젝트는 가상의 호텔 Haru(하루)를 홍보하며 예약 및 리뷰, 게시판 등의 서비스를 이용할 수 있는 호텔 예약 웹 사이트입니다.그린아트컴퓨터학원 Java 기반 풀스택 (스프링, Vue) 웹 개발자 양성과정의 세미 프로젝트로 진행하였으며,웹 개발을 위한 첫 협업을 한 프로젝트입니다.",
         periodAndTeam: "2023.11 - 2023.12 (1개월) . 5명",
         github: "https://github.com/rddckdeo/SemiProject",
-        picture: [
-            ""
-        ],
+        picture: [ haru1, haru2, haru3, haru4, haru5 ],
         skills: [
             "Java", "HTML", "CSS", "JavaScript", "Spring Framework", "JQuery", "Ajax", "BootStrap", "ORACLE", "MyBatis"
         ],
-        Tools: [
-            "Eclipse IDE", "VSCode", "Apache Tomcat", "GitHub"
+        tools: [
+            "Eclipse IDE", "VSCode", "Apache Tomcat", "GitHub", "DBeaver"
         ],
         roles: [
             "게시판 기능 추가",
@@ -24,22 +36,25 @@ export const projectDetails = [
         ]
     },
     {// CoCo
-        title : "",
-        subTitle: "",
-        content: "",
-        periodAndTeam: "",
-        github: "",
-        picture: [
-            ""
-        ],
+        title : "Collaboration Code (CoCo)",
+        subTitle: "개발자 협업을 위한 원스톱 프로젝트 관리 플랫폼",
+        content: "프로젝트 CoCo는 개발자들이 모여 프로젝트를 기획하고 팀원을 모집한 후 단일 페이지에서 협업에 필요한 여러 Tool을 사용하여 개발을 하는 홈페이지입니다. 캘린더 및 스케줄을 공유하여 일정을 공유하고 현재 작업에 진척도 및 설명이 필요할 경우 그림판을 통해 그림을 그린 후 내용을 공유할 수 있으며 실시간 채팅을 통해 회의 및 내용 공유를 할 수 있습니다. 또한 개발자들이 정보를 공유하거나 커뮤니케이션을 할 수 있는 정보게시판 또는 자유게시판이 제공되어 원활한 소통을 가능하게 합니다.",
+        periodAndTeam: "2024.01 - 2024.02 (1개월) . 3명",
+        github: "https://github.com/rddckdeo/FinalProject",
+        picture: [CoCo1, CoCo2, CoCo3, CoCo4, CoCo5, CoCo6],
         skills: [
-
+            "Spring Framework", "Java", "JSP", "HTML", "CSS", "JavaScript", "MyBatis", "ORACLE", "Spring Security"
         ],
-        Tools: [
-
+        tools: [
+            "STS3 - Eclipse", "VSCode", "DBeaver","Apache Tomcat", "GitHub"
         ],
         roles: [
-
+            "로그인, 회원가입 기능 전담 및 Spring Security를 사용한 비밀번호 암호화 복호화",
+            "Hero (Main) 페이지 및 로그인 상태 관리",
+            "Admin 페이지 전담 (통계 및 관리 - 방문자, 사용자, 프로젝트, 문의 등 )",
+            "게시판 댓글 및 답변 기능 추가,",
+            "My Page 기능 전담 (내 프로젝트 조회 및 관리 등 기능 구현)",
+            "알림 기능 추가 - 프로젝트에 댓글 및 답변, 상태에 따른 실시간 알림 기능 추가"
         ]
     },
     { // TravelPlan
